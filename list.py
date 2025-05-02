ec84bc3b7fc63cf29b86bd40941ed7cc6e76e216
Mohammed23200 <mgsa112233@gmail.com>
2025-05-02 21:40:49 +0300

    working with list

---
+++ list.py
@@ -0,0 +1,8 @@
+friends = ['john', 'filo', 'soso', 'Kareem',2,True]
+
+print(friends)#this will return all the elemnt in the list
+print(friends[2])#this will return soso because he return the index of the list
+print(friends[-1])# this will return 2 the bacl of the list
+print(friends[1:])#all the element from 1 to end
+print(friends[1:3])#from 1 to 3
+friends[-1] = 'salahe'#this will modifay the -1 element
