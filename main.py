195fae48dceb800c3a476690cc5666652237bfb4
Mohammed23200 <mgsa112233@gmail.com>
2025-05-02 20:54:31 +0300

    working with strings

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-name = "Jimmmy"
-age = 19
-print ("hello my name is " + name + " and I am " + str(age) + " years old")
+strs= "Hello World🌍"
+print(strs+" the first of the magic")
+print(strs.upper())#confirt to apper case .lower()->to make it lower case
+print(strs.islower())#gave us true if the string lower else false .isupper()
+print(strs.upper().isupper())#this will run the first function .upper so this will make the outpput true
+print(len(strs))#this len function take a string as parameter and return number of string
+print(strs[0])#when we want to get access to the chars we typpe the index in the square [] we start from 0 to len()
+print(strs.index("o"))#this will return the index of chars
+print(strs.replace("World","Geeks"))#this take two parameter one the what we want to replace and the second what we put instead in the string
