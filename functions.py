50c1f479c0f9a455810b59b0179e5470d54e8f28
Mohammed23200 <mgsa112233@gmail.com>
2025-05-03 13:45:47 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,9 @@
+def say_hi(): #when we want to delear a function we must use def name(parmeters):
+    print("Hi user this is my first function in python")#must be indented
+say_hi()
+def say_hi_to(name):
+    print("Hi "+name+" this is my first function in python")
+say_hi_to("Jimmmy")
+def add(num1,num2):
+    return num1+num2
+print(add(1982,2077))
